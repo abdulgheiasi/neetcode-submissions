a94e2de0d14403741e3bff4adf490ca871e1f555
Abdul Gheiasi <gheiasiabdul@gmail.com>
2026-07-20 17:22:21 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                prevTemp, prevIndex = stack.pop()
+                result[prevIndex] = i - prevIndex
+            
+            stack.append((temp, i))
+        
+        return result
